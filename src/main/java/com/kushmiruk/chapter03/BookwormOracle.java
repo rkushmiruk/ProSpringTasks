1fe730a9878a7091cab10bd5189741bb84c20cb4
Roman <rKushmiruk@gmail.com>
2017-07-27 20:08:02 +0300

    Refactor pom,log4 and gitignore files

--- src/main/java/com/kushmiruk/chapter03/BookwormOracle.java
+++ src/main/java/com/kushmiruk/chapter03/BookwormOracle.java
@@ -1,10 +1,4 @@
 package com.kushmiruk.chapter03;
 
-public class BookwormOracle implements Oracle {
-    private static final String message = "Encyclopedias are a waste of money - use the Internet";
-
-    @Override
-    public String defineMeaningOfLife() {
-        return message;
-    }
+public class BookwormOracle {
 }
--- src/main/java/com/kushmiruk/chapter03/XmlConfigWithBeanFactory.java
+++ src/main/java/com/kushmiruk/chapter03/XmlConfigWithBeanFactory.java
@@ -1,20 +1,4 @@
 package com.kushmiruk.chapter03;
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.core.io.ClassPathResource;
-
 public class XmlConfigWithBeanFactory {
-    private static final Logger LOGGER = Logger.getLogger(XmlConfigWithBeanFactory.class);
-    private static final String ORACLE_BEAN_NAME = "oracle";
-
-    public static void main(String[] args) {
-        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
-        reader.loadBeanDefinitions(new ClassPathResource("chapter03/META-INF/spring/xml-bean-factory-config"));
-
-        Oracle oracle = (Oracle) factory.getBean(ORACLE_BEAN_NAME);
-        LOGGER.info(oracle.defineMeaningOfLife());
-    }
 }
