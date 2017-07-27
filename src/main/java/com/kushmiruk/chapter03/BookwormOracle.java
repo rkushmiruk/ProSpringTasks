9a13f83946eac12a08ea06ce58d9d2e4fcdba587
Roman <rKushmiruk@gmail.com>
2017-07-27 20:05:38 +0300

    Add start of chapter02

--- src/main/java/com/kushmiruk/chapter03/BookwormOracle.java
+++ src/main/java/com/kushmiruk/chapter03/BookwormOracle.java
@@ -1,4 +1,10 @@
 package com.kushmiruk.chapter03;
 
-public class BookwormOracle {
+public class BookwormOracle implements Oracle {
+    private static final String message = "Encyclopedias are a waste of money - use the Internet";
+
+    @Override
+    public String defineMeaningOfLife() {
+        return message;
+    }
 }
--- src/main/java/com/kushmiruk/chapter03/Oracle.java
+++ src/main/java/com/kushmiruk/chapter03/Oracle.java
@@ -1,7 +1,6 @@
 package com.kushmiruk.chapter03;
 
-/**
- * Created by roman on 27.07.17.
- */
 public interface Oracle {
+
+    String defineMeaningOfLife();
 }
